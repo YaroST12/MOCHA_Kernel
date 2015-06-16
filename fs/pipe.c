18585aa65128f98bf77c97a2f958e5afc7bd7bda
Ben Hutchings <ben@decadent.org.uk>
2015-06-16 22:11:06 +0100

    pipe: iovec: Fix memory corruption when retrying atomic copy as non-atomic
    pipe_iov_copy_{from,to}_user() may be tried twice with the same iovec,
    the first time atomically and the second time not.  The second attempt
    needs to continue from the iovec position, pipe buffer offset and
    remaining length where the first attempt failed, but currently the
    pipe buffer offset and remaining length are reset.  This will corrupt
    the piped data (possibly also leading to an information leak between
    processes) and may also corrupt kernel memory.
    
    This was fixed upstream by commits f0d1bec9d58d ("new helper:
    copy_page_from_iter()") and 637b58c2887e ("switch pipe_read() to
    copy_page_to_iter()"), but those aren't suitable for stable.  This fix
    for older kernel versions was made by Seth Jennings for RHEL and I
    have extracted it from their update.
    
    CVE-2015-1805
    
    References: https://bugzilla.redhat.com/show_bug.cgi?id=1202855
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/pipe.c
+++ fs/pipe.c
@@ -365,15 +365,10 @@ pipe_read(struct kiocb *iocb, const struct iovec *_iov,
 			atomic = !iov_fault_in_pages_write(iov, chars);
 			remaining = chars;
 redo:
-			if (atomic)
-				addr = kmap_atomic(buf->page);
-			else
-				addr = kmap(buf->page);
-			error = pipe_iov_copy_to_user(iov, addr, &buf->offset, &remaining, atomic);
-			if (atomic)
-				kunmap_atomic(addr);
-			else
-				kunmap(buf->page);
+			addr = ops->map(pipe, buf, atomic);
+			error = pipe_iov_copy_to_user(iov, addr, &buf->offset,
+						      &remaining, atomic);
+			ops->unmap(pipe, buf, addr);
 			if (unlikely(error)) {
 				/*
 				 * Just retry with the slow path if we failed.
@@ -499,16 +494,10 @@ pipe_write(struct kiocb *iocb, const struct iovec *_iov,
 
 			iov_fault_in_pages_read(iov, chars);
 redo1:
-			if (atomic)
-				addr = kmap_atomic(buf->page);
-			else
-				addr = kmap(buf->page);
+			addr = ops->map(pipe, buf, atomic);
 			error = pipe_iov_copy_from_user(addr, &offset, iov,
 							&remaining, atomic);
-			if (atomic)
-				kunmap_atomic(addr);
-			else
-				kunmap(buf->page);
+			ops->unmap(pipe, buf, addr);
 			ret = error;
 			do_wakeup = 1;
 			if (error) {
