163a221abb5446be392a06d67487b22143a8f455
YaroST12 <yaro330@gmail.com>
2017-07-01 21:33:30 +0300

    intelliactive: a few tweaks

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -135,7 +135,7 @@ static unsigned int up_threshold_any_cpu_load = 65;
 static unsigned int sync_freq = 1044000;
 static unsigned int up_threshold_any_cpu_freq = 1044000;
 
-static int two_phase_freq_array[NR_CPUS] = {[0 ... NR_CPUS-1] = 1044000} ;
+static int two_phase_freq_array[NR_CPUS] = {[0 ... NR_CPUS-1] = 204000} ;
 
 static int cpufreq_governor_intelliactive(struct cpufreq_policy *policy,
 		unsigned int event);
