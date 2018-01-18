1423d97d1ceedc7bd17635e0e9619f3e22caa0b2
YaroST12 <yaro330@gmail.com>
2018-01-18 19:42:52 +0300

    Fix dat fokker
    Signed-off-by: YaroST12 <yaro330@gmail.com>

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -320,17 +320,10 @@ int pstore_mkfile(enum pstore_type_id type, char *psname, u64 id, int count,
 			  psname, id);
 		break;
 	case PSTORE_TYPE_CONSOLE:
-<<<<<<< HEAD
-		sprintf(name, "console-%s-%lld", psname, id);
-		break;
-	case PSTORE_TYPE_FTRACE:
-		sprintf(name, "ftrace-%s-%lld", psname, id);
-=======
 		scnprintf(name, sizeof(name), "console-%s", psname);
 		break;
 	case PSTORE_TYPE_FTRACE:
 		scnprintf(name, sizeof(name), "ftrace-%s", psname);
->>>>>>> 3797dc2... [PATCH v4 1/5] pstore: use snprintf
 		break;
 	case PSTORE_TYPE_MCE:
 		scnprintf(name, sizeof(name), "mce-%s-%lld", psname, id);
